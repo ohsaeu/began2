correct normalization

anal.py
@@ -47,7 +47,9 @@ def main():
        
     saver = tf.train.import_meta_graph(npz_path+'began2_model.ckpt.meta')
     saver.restore(sess, tf.train.latest_checkpoint(npz_path))
-  
+    
+    col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    
     g_idx=0
     e_idx=0
     d_idx=0                
@@ -55,7 +57,14 @@ def main():
         
         key = x.name.split(':')[0]
         scope = key.split('/')
-    
+        with tf.variable_scope(scope[0]) as vs1:
+            vs1.reuse_variables()
+            with tf.variable_scope(scope[1]) as vs2:
+                vs2.reuse_variables()
+                ref =tf.get_variable(scope[2], shape=x.shape) 
+                ref1=tf.assign(ref,x)
+                sess.run(ref1)
+        '''
         with tf.variable_scope(scope[0]) as vs1:
             vs1.reuse_variables()
             with tf.variable_scope(scope[1]) as vs2:
@@ -73,7 +82,7 @@ def main():
                     ref1=tf.assign(ref,d_params[d_idx])
                     sess.run(ref1)
                     d_idx+=1
-                    
+        '''            
 
     #z2 = tf.random_uniform((conf.n_batch, conf.n_img_out_pix,conf.n_img_out_pix,n_channel), minval=-1.0, maxval=1.0)
     z_fix =np.random.uniform(low=-1, high=1, size=(conf.n_batch, 64)).astype(np.float32)

main.py
@@ -28,8 +28,7 @@ def main():
     # execute generator
     g_net, g_vars = generate(z, conf.n_img_out_pix, conf.n_conv_hidden, n_channel,  is_train=True, reuse=False)
     g_img=tf.clip_by_value((g_net + 1)*127.5, 0, 255)
-    #g_img= tf.clip_by_value((g_net + 1)*127.5, 0, 255)
-
+    
     # execute discriminator
     e_g_net, e_vars = encode(g_net, conf.n_z, conf.n_img_out_pix, conf.n_conv_hidden, is_train=True, reuse=False)
     d_g_net, d_vars = decode(e_g_net, conf.n_z, conf.n_img_out_pix, conf.n_conv_hidden, n_channel, is_train=True, reuse=False)
@@ -99,7 +98,7 @@ def main():
     # sample_seed = np.random.uniform(low=-1, high=1, size=(config.sample_size, z_dim)).astype(np.float32)
 
     ##========================= TRAIN MODELS ================================## 
-    z_fix = np.random.uniform(-1, 1, size=(conf.n_batch, conf.n_z))
+    z_fix = np.random.uniform(0, 1, size=(conf.n_batch, conf.n_z))
 
     x_fix = data_files[0:conf.n_batch]
     x_fix=[get_image(f, conf.n_img_pix, is_crop=conf.is_crop, resize_w=conf.n_img_out_pix, is_grayscale = conf.is_gray) for f in x_fix]
@@ -135,7 +134,8 @@ def main():
             f_batch = data_files[idx*conf.n_batch:(idx+1)*conf.n_batch]
             data_batch = [get_image(f, conf.n_img_pix, is_crop=conf.is_crop, resize_w=conf.n_img_out_pix, is_grayscale = conf.is_gray) for f in f_batch]
             img_batch = np.array(data_batch).astype(np.float32)
-            img_batch = img_batch/127.5 - 1.
+            img_batch = tf.clip_by_value((img_batch + 1)*127.5, 0, 255)
+            #img_batch = img_batch/127.5 - 1.
 
             if conf.is_gray :
                 s,h,w = img_batch.shape